718f227657607afe0bf97fcda50d6961df77a57c
Ke Xie <kexi@amazon.com>
2018-09-14 23:20:48 -0700

    Add DataCutter

--- src/main/kotlin/club/antigfw/network/basic/surelink/DataCutter.kt
+++ src/main/kotlin/club/antigfw/network/basic/surelink/DataCutter.kt
@@ -1,5 +1,36 @@
 package club.antigfw.network.basic.surelink
 
+import kotlinx.coroutines.experimental.channels.Channel
+import java.nio.ByteBuffer
+import kotlin.math.min
+
+
 class DataCutter(val blockSize: Int) {
+    private val defaultQueueSize = 64
+    private val outgoingBuf = Channel<ByteArray>(defaultQueueSize)
+    private var buffer: ByteBuffer = ByteBuffer.allocate(blockSize)
+
+    suspend fun offer(bytes : ByteArray) : Boolean {
+        var read = 0
+        while (read < bytes.size) {
+            val len = min(bytes.size - read, buffer.remaining())
+            buffer.put(bytes, read, len)
+            if (!buffer.hasRemaining()) {
+                outgoingBuf.send(buffer.array())
+                buffer = ByteBuffer.allocate(blockSize)
+            }
+            read += len
+        }
+        return true
+    }
+
+    suspend fun close() {
+        if (buffer.position() > 0) {
+            outgoingBuf.send(buffer.array().sliceArray(0 until  buffer.position()))
+        }
+        outgoingBuf.close()
+    }
+
+    suspend fun poll() : ByteArray? = outgoingBuf.receiveOrNull()
 
 }
--- src/test/kotlin/club/antigfw/network/basic/surelink/DataCutterTest.kt
+++ src/test/kotlin/club/antigfw/network/basic/surelink/DataCutterTest.kt
@@ -1,7 +1,11 @@
 package club.antigfw.network.basic.surelink
 
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Assert
 import org.junit.BeforeClass
 import org.junit.Test
+import java.util.*
 
 class DataCutterTest {
     companion object {
@@ -13,6 +17,48 @@ class DataCutterTest {
 
     @Test
     fun testDataCutter() {
+        val rand = Random(System.currentTimeMillis())
+        val blockSize = rand.nextInt(100) + 1
+        println("blockSize is $blockSize")
+        val cutter = DataCutter(blockSize)
 
+        val send = launch {
+            var sent = 0
+            (0..10000).map { rand.nextInt(10000) + 1 }.forEach {
+                val bytes = ByteArray(it) { ((sent + it) % 256).toByte() }
+                cutter.offer(bytes)
+                sent += it
+            }
+            cutter.close()
+        }
+
+        val recv = launch {
+            var recvd = 0
+            var lastSize = -1
+            while (true) {
+                val bytes = cutter.poll() ?: break
+                if (lastSize > 0) {
+                    Assert.assertEquals(blockSize, lastSize)
+                }
+                (0 until bytes.size).forEach {
+                    Assert.assertEquals(bytes[it], ((recvd + it) % 256).toByte())
+                }
+                recvd += bytes.size
+                lastSize = bytes.size
+            }
+            println("lastBlockSize is $lastSize")
+        }
+
+        runBlocking {
+            send.join()
+            recv.join()
+        }
+    }
+
+    @Test
+    fun testDataCutter2() {
+        (0..10).forEach {
+            testDataCutter()
+        }
     }
 }
